8c2c8769f242200f23aac7c61dd030cda29ebfe0
王derful <11409153+wang-derful@user.noreply.gitee.com>
2022-09-14 11:39:12 +0800

    9/14

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -14,20 +14,47 @@
         <a href="###">秒杀</a>
       </nav>
       <div class="sort">
-        <div class="all-sort-list2">
-          <div class="item " v-for="(c1,index) in categoryList" :key="c1.categoryId" :class="{cur:currentIndex==index}">
-            <h3 @mouseenter="changeIndex(index)" @mouseleave="leaveIndex">
-              <a href="">{{c1.categoryName}}</a>
+        <!-- 路由跳转的事件委托 -->
+        <div class="all-sort-list2" @click="goSearch">
+          <div
+            class="item"
+            v-for="(c1, index) in categoryList"
+            :key="c1.categoryId"
+            :class="{ cur: currentIndex == index }"
+            @mouseleave="leaveIndex"
+          >
+            <h3 @mouseenter="changeIndex(index)">
+              <a
+                :data-categoryName="c1.categoryName"
+                :data-category1Id="c1.categoryId"
+                >{{ c1.categoryName }}</a
+              >
             </h3>
-            <div class="item-list clearfix">
-              <div class="subitem" v-for="c2 in c1.categoryChild" :key="c2.categoryId">
+            <!-- 二三级分类的显示与隐藏 -->
+            <div
+              class="item-list clearfix"
+              :style="{ display: currentIndex == index ? 'block' : 'none' }"
+            >
+              <div
+                class="subitem"
+                v-for="c2 in c1.categoryChild"
+                :key="c2.categoryId"
+              >
                 <dl class="fore">
                   <dt>
-                    <a href="">{{c2.categoryName}}</a>
+                    <a
+                      :data-categoryName="c2.categoryName"
+                      :data-category2Id="c2.categoryId"
+                      >{{ c2.categoryName }}</a
+                    >
                   </dt>
                   <dd>
                     <em v-for="c3 in c2.categoryChild" :key="c3.categoryId">
-                      <a href="">{{c3.categoryName}}</a>
+                      <a
+                        :data-categoryName="c3.categoryName"
+                        :data-category3Id="c3.categoryId"
+                        >{{ c3.categoryName }}</a
+                      >
                     </em>
                   </dd>
                 </dl>
@@ -41,157 +68,180 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex';
+import { mapState } from "vuex";
+import throttle from "lodash/throttle"; //按需引入节流
 export default {
-    name:'TypeNav',
-    data(){
-      return {
-        currentIndex:-1
+  name: "TypeNav",
+  data() {
+    return {
+      currentIndex: -1,
+    };
+  },
+  methods: {
+    changeIndex: throttle(function (index) {
+      //鼠标进入修改currentIndex属性，index为鼠标移上某一个一级分类的元素的索引值，进行节流
+      this.currentIndex = index;
+    }, 50), //当用户操作过快时，浏览器会反应不过来，如果函数里还要大量业务就会出现卡顿现象，所以需要节流
+    leaveIndex() {
+      this.currentIndex = -1;
+    },
+    goSearch(e) {
+      //三级联动路由跳转  编程式路由导航+路由传参
+      let element = e.target; //给1，2，3级别添加自定义属性后，获取自定义属性的a标签,用来区分其他子节点
+      // console.log(element);
+      // console.log(element.dataset);
+      let { categoryname, category1id, category2id, category3id } =element.dataset; //如果标签属性上有categoryname那么一定是a标签
+      if (categoryname) {//一级二级三级分类的a标签
+        let location = { name: "search" };
+        let query = { categoryName: categoryname };
+        if (category1id) {
+          //整理路由跳转的参数
+          query.category1Id = category1id;
+        } else if (category2id) {
+          query.category2Id = category2id;
+        } else {
+          query.category3Id = category3id;
+        }
+        location.query = query //整理路由参数
+        this.$router.push(location) //进行路由传参
       }
     },
-    methods:{
-      changeIndex(index) { //鼠标进入修改currentIndex属性，index为鼠标移上某一个一级分类的元素的索引值
-        this.currentIndex = index
-      },
-      leaveIndex() {
-        this.currentIndex = -1
-      }
-    },
-    mounted(){
-      //组件挂载完毕发送请求
-      this.$store.dispatch('categoryList')
-    },
-    computed:{
-      ...mapState({//右侧需要的是一个函数，当使用计算属性时，右侧函数会立即执行一次
-        //注入一个参数state，其实即为大仓库中的数据
-        categoryList:state => state.home.categoryList
-      })
-    }
+  },
+  mounted() {
+    //组件挂载完毕发送请求
+    this.$store.dispatch("categoryList");
+  },
+  computed: {
+    ...mapState({
+      //右侧需要的是一个函数，当使用计算属性时，右侧函数会立即执行一次
+      //注入一个参数state，其实即为大仓库中的数据
+      categoryList: (state) => state.home.categoryList,
+    }),
+  },
 };
 </script>
 
 <style scoped lang="less">
-    .type-nav {
-        border-bottom: 2px solid #e1251b;
+.type-nav {
+  border-bottom: 2px solid #e1251b;
 
-        .container {
-            width: 1200px;
-            margin: 0 auto;
-            display: flex;
-            position: relative;
+  .container {
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    position: relative;
 
-            .all {
-                width: 210px;
-                height: 45px;
-                background-color: #e1251b;
-                line-height: 45px;
-                text-align: center;
-                color: #fff;
-                font-size: 14px;
-                font-weight: bold;
-            }
-
-            .nav {
-                a {
-                    height: 45px;
-                    margin: 0 22px;
-                    line-height: 45px;
-                    font-size: 16px;
-                    color: #333;
-                }
-            }
-
-            .sort {
-                position: absolute;
-                left: 0;
-                top: 45px;
-                width: 210px;
-                height: 461px;
-                position: absolute;
-                background: #fafafa;
-                z-index: 999;
-
-                .all-sort-list2 {
-                    .cur {
-                      background-color: skyblue;
-                    }
-                    .item {
-                        h3 {
-                            line-height: 30px;
-                            font-size: 14px;
-                            font-weight: 400;
-                            overflow: hidden;
-                            padding: 0 20px;
-                            margin: 0;
-
-                            a {
-                                color: #333;
-                            }
-                        }
-
-                        .item-list {
-                            display: none;
-                            position: absolute;
-                            width: 734px;
-                            min-height: 460px;
-                            background: #f7f7f7;
-                            left: 210px;
-                            border: 1px solid #ddd;
-                            top: 0;
-                            z-index: 9999 !important;
-
-                            .subitem {
-                                float: left;
-                                width: 650px;
-                                padding: 0 4px 0 8px;
-
-                                dl {
-                                    border-top: 1px solid #eee;
-                                    padding: 6px 0;
-                                    overflow: hidden;
-                                    zoom: 1;
-
-                                    &.fore {
-                                        border-top: 0;
-                                    }
-
-                                    dt {
-                                        float: left;
-                                        width: 54px;
-                                        line-height: 22px;
-                                        text-align: right;
-                                        padding: 3px 6px 0 0;
-                                        font-weight: 700;
-                                    }
-
-                                    dd {
-                                        float: left;
-                                        width: 415px;
-                                        padding: 3px 0 0;
-                                        overflow: hidden;
-
-                                        em {
-                                            float: left;
-                                            height: 14px;
-                                            line-height: 14px;
-                                            padding: 0 8px;
-                                            margin-top: 5px;
-                                            border-left: 1px solid #ccc;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        &:hover {
-                            .item-list {
-                                display: block;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    .all {
+      width: 210px;
+      height: 45px;
+      background-color: #e1251b;
+      line-height: 45px;
+      text-align: center;
+      color: #fff;
+      font-size: 14px;
+      font-weight: bold;
     }
 
+    .nav {
+      a {
+        height: 45px;
+        margin: 0 22px;
+        line-height: 45px;
+        font-size: 16px;
+        color: #333;
+      }
+    }
+
+    .sort {
+      position: absolute;
+      left: 0;
+      top: 45px;
+      width: 210px;
+      height: 461px;
+      position: absolute;
+      background: #fafafa;
+      z-index: 999;
+
+      .all-sort-list2 {
+        .cur {
+          background-color: skyblue;
+        }
+        .item {
+          h3 {
+            line-height: 30px;
+            font-size: 14px;
+            font-weight: 400;
+            overflow: hidden;
+            padding: 0 20px;
+            margin: 0;
+
+            a {
+              color: #333;
+            }
+          }
+
+          .item-list {
+            display: none;
+            position: absolute;
+            width: 734px;
+            min-height: 460px;
+            background: #f7f7f7;
+            left: 210px;
+            border: 1px solid #ddd;
+            top: 0;
+            z-index: 9999 !important;
+
+            .subitem {
+              float: left;
+              width: 650px;
+              padding: 0 4px 0 8px;
+
+              dl {
+                border-top: 1px solid #eee;
+                padding: 6px 0;
+                overflow: hidden;
+                zoom: 1;
+
+                &.fore {
+                  border-top: 0;
+                }
+
+                dt {
+                  float: left;
+                  width: 54px;
+                  line-height: 22px;
+                  text-align: right;
+                  padding: 3px 6px 0 0;
+                  font-weight: 700;
+                }
+
+                dd {
+                  float: left;
+                  width: 415px;
+                  padding: 3px 0 0;
+                  overflow: hidden;
+
+                  em {
+                    float: left;
+                    height: 14px;
+                    line-height: 14px;
+                    padding: 0 8px;
+                    margin-top: 5px;
+                    border-left: 1px solid #ccc;
+                  }
+                }
+              }
+            }
+          }
+
+          // &:hover {
+          //     .item-list {
+          //         display: block;
+          //     }
+          // }
+        }
+      }
+    }
+  }
+}
 </style>
